a59dcd4a71d91b9cb3c720f0e2b8d57089d6e68d
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-10-13 17:23:47 +0200

    ENH: add option to input segment image apart from radius value

--- Modules/Applications/AppClassification/app/otbClassificationMapRegularization.cxx
+++ Modules/Applications/AppClassification/app/otbClassificationMapRegularization.cxx
@@ -5,14 +5,15 @@
   Version:   $Revision$
 
 
-  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  Copyright (c) Centre National d'Etudes Spatiales,
+  Copyright (c) 2015, CS Systemes d'Information.
+
+  All rights reserved.
   See OTBCopyright.txt for details.
 
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notices for more information.
-
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+   PURPOSE.  See the above copyright notices for more information.
 =========================================================================*/
 
 // Wrappers
@@ -22,8 +23,20 @@
 // Majority Voting filter includes
 #include "otbNeighborhoodMajorityVotingImageFilter.h"
 
+#include "itkAttributeLabelObject.h"
+#include "itkLabelMap.h"
+#include <itkLabelImageToLabelMapFilter.h>
+#include <itkLabelMapToAttributeImageFilter.h>
+#include "otbLabelMapWithMajorityClassLabelFilter.h"
+
 namespace otb
 {
+enum
+{
+  Method_Radius = 0,
+  Method_Image
+};
+
 namespace Wrapper
 {
 
@@ -43,6 +56,7 @@ public:
 
   /** Filters typedef */
   typedef UInt16ImageType IOLabelImageType;
+  typedef UInt16ImageType::InternalPixelType LabelType;
 
   // Neighborhood majority voting filter type
   typedef otb::NeighborhoodMajorityVotingImageFilter<IOLabelImageType> NeighborhoodMajorityVotingFilterType;
@@ -50,6 +64,15 @@ public:
   // Binary ball Structuring Element type
   typedef NeighborhoodMajorityVotingFilterType::KernelType StructuringType;
   typedef StructuringType::RadiusType RadiusType;
+
+
+  typedef itk::AttributeLabelObject<LabelType, 2, LabelType>  LabelObjectType; //1st attrib : segmentation type, 3rd attrib : classification type
+  typedef itk::LabelMap<LabelObjectType>				LabelMapType;
+
+  typedef itk::LabelImageToLabelMapFilter<IOLabelImageType, LabelMapType > 		LabelImageToLabelMapFilterType;
+  typedef itk::LabelMapToAttributeImageFilter<LabelMapType, IOLabelImageType > 		LabelMapToAttributeImageFilterType;
+  typedef otb::LabelMapWithMajorityClassLabelFilter<LabelMapType, IOLabelImageType >  LabelMapWithMajorityClassLabelFilterType;
+
 
 
 private:
@@ -68,7 +91,6 @@ private:
     AddDocTag(Tags::Learning);
     AddDocTag(Tags::Analysis);
 
-
     /** GROUP IO CLASSIFICATION */
     AddParameter(ParameterType_Group,"io","Input and output images");
     SetParameterDescription("io","This group of parameters allows to set input and output images for classification map regularization by Majority Voting.");
@@ -80,20 +102,42 @@ private:
     SetParameterDescription( "io.out", "The output regularized labeled image.");
     SetParameterOutputImagePixelType( "io.out", ImagePixelType_uint8);
 
-
+    /** GROUP Regularization parameters */
     AddParameter(ParameterType_Group,"ip","Regularization parameters");
-    SetParameterDescription("ip","This group allows to set parameters for classification map regularization by Majority Voting.");
+    SetParameterDescription("ip","This group allows to set common parameters for classification map regularization by Majority Voting.");
 
-    AddParameter(ParameterType_Int, "ip.radius", "Structuring element radius (in pixels)");
-    SetParameterDescription("ip.radius", "The radius of the ball shaped structuring element (expressed in pixels). By default, 'ip.radius = 1 pixel'.");
-    SetDefaultParameterInt("ip.radius", 1.0);
+    AddParameter(ParameterType_Choice, "ip.method", "Select majority voting input");
+    SetParameterDescription("ip.method", "Selection of the  input for majority voting.(radius/segmentation image).");
+
+    // Radius
+    AddChoice("ip.method.radius","Input radius");
+    SetParameterDescription("ip.method.radius","Perform regularization based on given radius.");
+    AddParameter(ParameterType_Int, "ip.method.radius.value", "Structuring element radius (in pixels)");
+    SetParameterDescription("ip.method.radius.value", "The radius of the ball shaped structuring element (expressed in pixels). By default, 'ip.radius = 1 pixel'.");
+    SetDefaultParameterInt("ip.method.radius.value", 1.0);
+
+    // segmentation image
+    AddChoice("ip.method.image","Input Image (Segmentation)");
+    SetParameterDescription("ip.method.image","Perform regularization based on given input segmentation image");
+
+    AddParameter(ParameterType_InputImage, "ip.method.image.value",     "Input Image (Segmentation)");
+    SetParameterDescription( "ip.method.image.value", "Input 2 : segmentation provided by a label image");
+
+    //method.segment.
+    AddParameter(ParameterType_Int, "ip.method.image.nodatavalue", "The label that corresponds to no data within the segmentation image");
+    SetParameterDescription("ip.method.image.nodatavalue", "The label that corresponds to no data within the segmentation image");
+    SetDefaultParameterInt("ip.method.image.nodatavalue", 0);
+    SetMinimumParameterIntValue("ip.method.image.nodatavalue", 0);
+    MandatoryOff("ip.method.image.nodatavalue");
+
 
     AddParameter(ParameterType_Empty, "ip.suvbool", "Multiple majority: Undecided(X)/Original");
     SetParameterDescription("ip.suvbool", "Pixels with more than 1 majority class are marked as Undecided if this parameter is checked (true), or keep their Original labels otherwise (false). Please note that the Undecided value must be different from existing labels in the input labeled image. By default, 'ip.suvbool = false'.");
 
-    AddParameter(ParameterType_Int, "ip.nodatalabel", "Label for the NoData class");
-    SetParameterDescription("ip.nodatalabel", "Label for the NoData class. Such input pixels keep their NoData label in the output image. By default, 'ip.nodatalabel = 0'.");
-    SetDefaultParameterInt("ip.nodatalabel", 0.0);
+     AddParameter(ParameterType_Int, "ip.nodatalabel", "The label that corresponds to no data within the classification image");
+     SetParameterDescription("ip.nodatalabel", "The label that corresponds to no data within the classification image. Such input pixels keep their NoData label in the output image. By default, 'ip.nodatalabel = 0'.");
+     SetDefaultParameterInt("ip.nodatalabel", 0.0);
+
 
     AddParameter(ParameterType_Int, "ip.undecidedlabel", "Label for the Undecided class");
     SetParameterDescription("ip.undecidedlabel", "Label for the Undecided class. By default, 'ip.undecidedlabel = 0'.");
@@ -105,9 +149,9 @@ private:
     // Doc example parameter settings
     SetDocExampleParameterValue("io.in", "clLabeledImageQB123_1.tif");
     SetDocExampleParameterValue("io.out", "clLabeledImageQB123_1_CMR_r2_nodl_10_undl_7.tif");
-    SetDocExampleParameterValue("ip.radius", "2");
+    SetDocExampleParameterValue("ip.method.radius.value", "2");
+    SetDocExampleParameterValue("ip.nodatalabel", "4");
     SetDocExampleParameterValue("ip.suvbool", "true");
-    SetDocExampleParameterValue("ip.nodatalabel", "10");
     SetDocExampleParameterValue("ip.undecidedlabel", "7");
   }
 
@@ -124,39 +168,77 @@ private:
     // Load input labeled image to regularize
     UInt16ImageType::Pointer inImage = GetParameterUInt16Image("io.in");
 
-    // Neighborhood majority voting filter settings
-    RadiusType rad;
-    rad[0] = GetParameterInt("ip.radius");
-    rad[1] = GetParameterInt("ip.radius");
-
-    StructuringType seBall;
-    seBall.SetRadius(rad);
-    seBall.CreateStructuringElement();
-    m_NeighMajVotingFilter->SetKernel(seBall);
-
-    m_NeighMajVotingFilter->SetInput(inImage);
-    m_NeighMajVotingFilter->SetLabelForNoDataPixels(GetParameterInt("ip.nodatalabel"));
-    m_NeighMajVotingFilter->SetLabelForUndecidedPixels(GetParameterInt("ip.undecidedlabel"));
-
-    // Set to Undecided label if NOT unique Majority Voting
-    if (IsParameterEnabled("ip.suvbool"))
+    if (GetParameterInt("ip.method") == Method_Radius)
       {
-      m_NeighMajVotingFilter->SetKeepOriginalLabelBool(false);
+      otbAppLogINFO("Majority Voting with input radius");
+
+      // Neighborhood majority voting filter settings
+      RadiusType rad;
+      rad.Fill(GetParameterInt("ip.method.radius.value"));
+
+      StructuringType seBall;
+      seBall.SetRadius(rad);
+      seBall.CreateStructuringElement();
+      m_NeighMajVotingFilter->SetKernel(seBall);
+
+      m_NeighMajVotingFilter->SetInput(inImage);
+      m_NeighMajVotingFilter->SetLabelForNoDataPixels(GetParameterInt("ip.nodatalabel"));
+      m_NeighMajVotingFilter->SetLabelForUndecidedPixels(GetParameterInt("ip.undecidedlabel"));
+
+      // Set to Undecided label if NOT unique Majority Voting
+      if (IsParameterEnabled("ip.suvbool"))
+        {
+        m_NeighMajVotingFilter->SetKeepOriginalLabelBool(false);
+        }
+      // Keep Original label value if NOT unique Majority Voting
+      else
+        {
+        m_NeighMajVotingFilter->SetKeepOriginalLabelBool(true);
+        }
+
+      /** REGULARIZATION OF CLASSIFICATION */
+      SetParameterOutputImage<IOLabelImageType>("io.out", m_NeighMajVotingFilter->GetOutput());
       }
-    // Keep Original label value if NOT unique Majority Voting
+    else if (GetParameterInt("ip.method") == Method_Image)
+      {
+      otbAppLogINFO("Majority Voting with input segmentation image");
+
+     UInt16ImageType::Pointer inputSegImage = GetParameterUInt16Image("ip.method.image.value");
+
+     //Instanciations
+     m_LabelImageToLabelMapFilter = LabelImageToLabelMapFilterType::New();
+     m_LabelMapToAttributeImageFilter = LabelMapToAttributeImageFilterType::New();
+     m_LabelMapWithMajorityClassLabelFilter = LabelMapWithMajorityClassLabelFilterType::New();
+
+     m_LabelImageToLabelMapFilter->SetInput(inputSegImage);
+
+     m_LabelMapWithMajorityClassLabelFilter->SetInput(m_LabelImageToLabelMapFilter->GetOutput());
+     m_LabelMapWithMajorityClassLabelFilter->SetClassifImage(inImage);
+     //Nodata
+     m_LabelMapWithMajorityClassLabelFilter->SetNoDataSegValue(GetParameterInt("ip.method.image.nodatavalue"));
+     m_LabelMapWithMajorityClassLabelFilter->SetNoDataClassifValue(GetParameterInt("ip.nodatalabel"));
+
+     //m_Attribute2Image
+     m_LabelMapToAttributeImageFilter->SetInput(m_LabelMapWithMajorityClassLabelFilter->GetOutput());
+
+     //set output image
+     SetParameterOutputImage<IOLabelImageType>("io.out", m_LabelMapToAttributeImageFilter->GetOutput());
+   }
+
     else
       {
-      m_NeighMajVotingFilter->SetKeepOriginalLabelBool(true);
+      otbAppLogINFO("Unknow method selected.");
       }
-
-    /** REGULARIZATION OF CLASSIFICATION */
-    SetParameterOutputImage<IOLabelImageType>("io.out", m_NeighMajVotingFilter->GetOutput());
 
   }// END DoExecute()
 
 
   NeighborhoodMajorityVotingFilterType::Pointer m_NeighMajVotingFilter;
-}; // END class ClassificationMapRegularization
+  LabelImageToLabelMapFilterType::Pointer m_LabelImageToLabelMapFilter;
+  LabelMapToAttributeImageFilterType::Pointer  m_LabelMapToAttributeImageFilter;
+  LabelMapWithMajorityClassLabelFilterType::Pointer m_LabelMapWithMajorityClassLabelFilter;
+
+};//end class
 
 
 }// END namespace wrapper
